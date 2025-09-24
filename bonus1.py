eb111c03600c66d2fb994e89f32a8fb61524d1b0
Shivansh Sharma <shivanshsharma4272@gmail.com>
2025-09-24 13:24:02 +0530

    Update bonus1.py

--- bonus1.py
+++ bonus1.py
@@ -1,25 +1,30 @@
 n=int(input("Enter number of bulbs: "))
 p=int(input("Enter number of pass toggled: "))
 
-if p==1:
-    print("Every Bulb is ON.")
+if n>0:
+   
 
-elif p==2:
-    for i in range(1,n+1):
-       if i//2==i/2:
-        print(i,"on")
+    if p==1:
+        print("Every Bulb is ON.")
 
-       elif i//2!=i/2:
-        print(i,"OFF")
+    elif p==2:
+        for i in range(1,n+1):
+         if i//2==i/2:
+            print(i,"on")
 
-elif p==3:
-   for i in range(1,n+1):
-      if i//3==i/3:
-        print(i,"on")
+         elif i//2!=i/2:
+            print(i,"OFF")
 
-      elif i//3!=i/3:
-        print(i,"OFF")
+    elif p==3:
+        for i in range(1,n+1):
+            if i//3==i/3:
+                print(i,"on")
+
+            elif i//3!=i/3:
+                print(i,"OFF")
+
+    else:
+        print("Enter a valid pass!")
 
 else:
-   print("Enter a valid pass!")
-
+    print("Enter A valid number of bulbs.")
