a29a12799d78701efa213ef45015e5d53b4849dc
Shivansh Sharma <shivanshsharma4272@gmail.com>
2025-09-24 11:52:36 +0530

    Add files via upload

---
+++ q7.py
@@ -0,0 +1,22 @@
+s=int(input("enter your score:"))
+r=int(input("Do you have a special reccomendation? (Type 1 for yes and 0 for no)"))
+
+if r==1:
+    if s>50:
+        print("You are eligible!")
+
+    elif s==100:
+        print("You are a master!")
+
+elif r==0:
+    if s>70:
+        print("You are eligible!")
+
+    elif s==100:
+        print("You are a master!")
+
+elif s<50:
+    print("You are not eligible!")
+
+else:
+    print()
