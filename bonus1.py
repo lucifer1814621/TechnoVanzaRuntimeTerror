681862815d858706861550314adcac648edc8d30
Shivansh Sharma <shivanshsharma4272@gmail.com>
2025-09-24 13:18:51 +0530

    Add files via upload

---
+++ bonus1.py
@@ -0,0 +1,25 @@
+n=int(input("Enter number of bulbs: "))
+p=int(input("Enter number of pass toggled: "))
+
+if p==1:
+    print("Every Bulb is ON.")
+
+elif p==2:
+    for i in range(1,n+1):
+       if i//2==i/2:
+        print(i,"on")
+
+       elif i//2!=i/2:
+        print(i,"OFF")
+
+elif p==3:
+   for i in range(1,n+1):
+      if i//3==i/3:
+        print(i,"on")
+
+      elif i//3!=i/3:
+        print(i,"OFF")
+
+else:
+   print("Enter a valid pass!")
+
