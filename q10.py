a0de658ae3d9b8702a51535e4e94bd2de47f49e7
Shivansh Sharma <shivanshsharma4272@gmail.com>
2025-09-24 12:09:23 +0530

    Add files via upload

---
+++ q10.py
@@ -0,0 +1,9 @@
+x=int(input("enter a number:"))
+y=int(input("enter another number:"))
+z=int(input("enter another number:"))
+
+if x+y+z!=45 and x==10:
+    print("PASS!")
+
+else:
+    print("You cannot pass!")
